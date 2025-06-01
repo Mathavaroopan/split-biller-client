bb95dccaaf8a88bb5dc47e3c8aac7f1fb12fa236
mathavaroopan <roopanmathava@gmail.com>
2025-06-01 19:22:06 +0530

    trying to build

--- src/pages/GroupDetails.tsx
+++ src/pages/GroupDetails.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import api from '../utils/api';
-import UserStats from '../components/UserStats';
 
 interface User {
   _id: string;
